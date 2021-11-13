5285e8a3f3c2f29b9942ddc80c608c6523f455d6
Vishal Sharma <vish.sharma0203@gmail.com>
2021-11-13 21:51:54 +0530

    basic refactor and console colors

--- packages/webapp/src/apps/Home/Home.tsx
+++ packages/webapp/src/apps/Home/Home.tsx
@@ -1,25 +1,14 @@
 import React, { FC } from 'react';
-import { MyPromise } from '@codealong/utilities';
 import styles from './Home.module.scss';
 
 type HomeProps = {
   message?: string;
 };
 
-const Home: FC<HomeProps> = ({ message = 'Code along with Vishal' }) => {
-  new MyPromise((resolve: any) => {
-    setTimeout(() => {
-      resolve(10);
-    }, 1000);
-  }).then((data: number) => {
-    console.log(data);
-  });
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.title}>{message}</div>
-    </div>
-  );
-};
+const Home: FC<HomeProps> = ({ message = 'Code along with Vishal' }) => (
+  <div className={styles.container}>
+    <div className={styles.title}>{message}</div>
+  </div>
+);
 
 export default Home;
